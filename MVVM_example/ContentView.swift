4dfad4ca61fd63ab339d53e40a82dc84f3594e75
emm <723125@gmail.com>
2021-02-15 11:24:46 +0000

    Initial Commit

---
+++ MVVM_example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM_example
+//
+//  Created by emm on 15/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM_example/MVVM_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_exampleApp.swift
+//  MVVM_example
+//
+//  Created by emm on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
